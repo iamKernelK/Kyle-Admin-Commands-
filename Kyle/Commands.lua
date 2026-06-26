75116b4660242fad593b0cc67fb40587ebc4f4cf
Kyle <krarhader748@gmail.com>
2026-06-26 16:55:32 +0300

    Update Commands.lua

--- Kyle/Commands.lua
+++ Kyle/Commands.lua
@@ -1,1 +1,14 @@
+-- ModuleScript: Commands
+local Commands = {}
 
+Commands["Speed"] = {
+    Action = function(val) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val end,
+    Description = "Change walk speed (Default: 16)"
+}
+
+Commands["JumpPower"] = {
+    Action = function(val) game.Players.LocalPlayer.Character.Humanoid.JumpPower = val end,
+    Description = "Change jump height (Default: 50)"
+}
+
+return Commands
