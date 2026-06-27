2688d7c2d4baf0c30ea27160ddc652e76c94694f
Kyle <krarhader748@gmail.com>
2026-06-27 10:00:00 +0300

    Add files via upload

---
+++ Kyle/Commands.lua
@@ -0,0 +1,26 @@
+local Commands = {}
+
+Commands["Speed"] = {
+    Action = function(val)
+        val = tonumber(val) or 16
+        local char = game.Players.LocalPlayer.Character
+        if char and char:FindFirstChild("Humanoid") then
+            char.Humanoid.WalkSpeed = val
+        end
+    end,
+    Description = "Change walk speed (Default: 16)"
+}
+
+Commands["JumpPower"] = {
+    Action = function(val)
+        val = tonumber(val) or 50
+        local char = game.Players.LocalPlayer.Character
+        if char and char:FindFirstChild("Humanoid") then
+            char.Humanoid.UseJumpPower = true
+            char.Humanoid.JumpPower = val
+        end
+    end,
+    Description = "Change jump height (Default: 50)"
+}
+
+return Commands
